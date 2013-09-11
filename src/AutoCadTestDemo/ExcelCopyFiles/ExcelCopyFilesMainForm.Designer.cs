53394c982247843d9219c7867a34f0f2fd91a5f4
wxu <wxu@huayancreate.com>
2013-09-11 23:00:50 +0800

    更新

--- src/AutoCadTestDemo/ExcelCopyFiles/ExcelCopyFilesMainForm.Designer.cs
+++ src/AutoCadTestDemo/ExcelCopyFiles/ExcelCopyFilesMainForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ExcelCopyFilesMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Name = "ExcelCopyFilesMainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
